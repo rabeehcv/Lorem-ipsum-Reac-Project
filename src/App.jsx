b20dd8172db294c614c6c95d7ef3e174b250f78b
rabeehcv <rabeehcv28@gmail.com>
2024-06-25 11:38:38 +0530

    update project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
+import data from './data';
 
 function App(){
+  const [count,setCount] = useState(0);
+  const [text,setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello World");
+  }
   return(
-    <h2>Project Setup</h2>
+    <section className="section-center">
+      <h3>Lorem Ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input type="number" name="amount" id="amount" value={count} onChange={(e)=>setCount(e.target.value)} />
+        <button type="submit" className="btn">Generate</button>
+      </form>
+    </section>
   )
 }
 
