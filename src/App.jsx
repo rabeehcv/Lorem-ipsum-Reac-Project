2ca014a0c5b07c466f8ae2bfc395faa40d51c2b5
rabeehcv <rabeehcv28@gmail.com>
2024-06-26 13:54:49 +0530

    add: final project updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,26 @@ function App(){
   const [text,setText] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello World");
+    let amount = parseInt(count); //convert string type to integer
+    if(count<0){
+      amount=1;
+    }
+    if(count>9){
+      amount=9;
+    }
+    setText(data.slice(0,amount));
   }
   return(
     <section className="section-center">
-      <h3>Lorem Ipsum</h3>
+      <h3>Lorem Ipsum (Generate Upto 9 Paragraphs)</h3>
       <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">Paragraphs:</label>
         <input type="number" name="amount" id="amount" value={count} onChange={(e)=>setCount(e.target.value)} />
         <button type="submit" className="btn">Generate</button>
       </form>
+      {text.map((item,index)=>{
+        return <p key={index}>{item}</p>
+      })}
     </section>
   )
 }
